55d1098f88838dd009380e4ffded8b337d3a3b87
Maxim Bazuev <bazuka5801@mail.ru>
2023-05-26 01:08:23 +0300

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Foo from './Foo.vue';
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(null)
+</script>
+
+<template>
+  <Foo ref="count" :foo="count" />
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
